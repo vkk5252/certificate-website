793858a78a8e2b50bbb00be8b80f1dc5da4e52ef
Vincent Kwan <vkk@vincents-mbp.lan>
2023-04-23 12:57:11 -0400

    .

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -3,7 +3,7 @@ import getClientIndexPath from "../config/getClientIndexPath.js";
 
 const router = new express.Router();
 
-const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/verify", "/reset-password", "/home", "/home/employer", "user-session/new/employer", "/verify-email", "/employee-grid", "/forgot-password"];
+const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/verify", "/reset-password", "/home", "/home/employer", "user-session/new/employer", "/verify-email", "/employee-grid", "/forgot-password", "/forgot-password/email-sent"];
 router.get(clientRoutes, (req, res) => {
   res.sendFile(getClientIndexPath());
 });
