8f48278b80731dceee8ab17f6bc0d4c023cd37a2
Vincent Kwan <vkk@vincents-mbp.lan>
2023-04-28 10:54:31 -0400

    removed more PostgreSQL things

--- server/src/config.js
+++ server/src/config.js
@@ -1,11 +1,9 @@
 import "./boot.js";
 import getNodeEnv from "./config/getNodeEnv.js";
-import getDatabaseUrl from "./config/getDatabaseUrl.cjs";
 
 export default {
   nodeEnv: getNodeEnv(),
   session: { secret: process.env.SESSION_SECRET },
-  databaseUrl: getDatabaseUrl(getNodeEnv()),
   web: { host: process.env.HOST || "0.0.0.0", port: process.env.PORT || 3000 },
   emailVerificationMaxAge: 1 * 60 * 1000,
   passwordResetMaxAge: 5 * 60 * 1000,
